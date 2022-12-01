9a5b7e6d2f27160f893bc3c64cf1ccde1fe41d89
Kkkkkkkkun <william_zjk111@163.com>
2022-12-01 20:24:14 +0800

    项目第一天

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import tabBar from './components/tab-bar/tab-bar.vue';
+</script>
+
+<template>
+<div class="app">
+  <router-view/>
+  <tab-bar></tab-bar>
+</div>
+</template>
+
+<style scoped>
+
+</style>
