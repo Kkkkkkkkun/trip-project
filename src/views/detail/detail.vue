94272251118651581d4b545b7da3a21fc857dfbb
Kkkkkkkkun <william_zjk111@163.com>
2023-04-28 17:37:53 +0800

    详情页facility

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -4,7 +4,7 @@ import { getDetailInfos } from '@/service'
 import { computed, ref } from 'vue';
 import DetailSwipe from './cpns/detail-swipe.vue'
 import DetailInfos from './cpns/detail-infos.vue'
-import detailSection from '@/components/detail-section/detail-section.vue';
+import detailFacility from './cpns/detail-facility.vue';
 
 
 const router = useRouter()
@@ -37,11 +37,7 @@ const onClickLeft = () => {
     <div class="main" v-if="mainPart">
         <DetailSwipe :swipe-data="mainPart.topModule.housePicture.housePics" />
         <DetailInfos :top-infos="mainPart.topModule"/>
-        <div class="facility">
-          <detail-section title="房屋设施" moreText="查看全部内容">
-            <button>111111</button>
-          </detail-section>
-        </div>
+        <detailFacility :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
     </div>
   </div>
 </template>
